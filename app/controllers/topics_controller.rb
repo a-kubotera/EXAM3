340c44ec92df8c5105f4e68b01a115f113ac2f48
a-kubotera <akira.kubotera+github@gmail.com>
2017-08-26 19:00:28 +0900

    デザイン一段落

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -6,6 +6,7 @@ class TopicsController < ApplicationController
   # GET /topics.json
   def index
     @topics = Topic.all
+
   end
 
   # GET /topics/1
@@ -32,7 +33,7 @@ class TopicsController < ApplicationController
     @topic.user_id = current_user.id
     respond_to do |format|
       if @topic.save
-        format.html { redirect_to @topic, notice: 'Topic was successfully created.' }
+        format.html { redirect_to @topic, notice: '新しいトピックを作成しました！' }
         format.json { render :show, status: :created, location: @topic }
       else
         format.html { render :new }
@@ -46,7 +47,7 @@ class TopicsController < ApplicationController
   def update
     respond_to do |format|
       if @topic.update(topic_params)
-        format.html { redirect_to @topic, notice: 'Topic was successfully updated.' }
+        format.html { redirect_to @topic, notice: 'トピックの更新が完了しました！' }
         format.json { render :show, status: :ok, location: @topic }
       else
         format.html { render :edit }
@@ -60,7 +61,7 @@ class TopicsController < ApplicationController
   def destroy
     @topic.destroy
     respond_to do |format|
-      format.html { redirect_to topics_url, notice: 'Topic was successfully destroyed.' }
+      format.html { redirect_to topics_url, notice: 'トピックを削除しました！' }
       format.json { head :no_content }
     end
   end
