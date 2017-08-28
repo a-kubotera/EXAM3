40d7f77a5376091da4c596f16edc9e5b88e4f859
a-kubotera <akira.kubotera+github@gmail.com>
2017-08-28 14:14:57 +0900

    修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,13 +11,14 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require_tree .
+
 //= require_tree ./material-design
 //= require jquery2
 //= require jquery_ujs
 //= require bootstrap-material-design
 //= require bootstrap-sprockets
 //= require_self
+//= require_tree .
 
 $(document).on('ready page:load', function() {
   $.material.init();
