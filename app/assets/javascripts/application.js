44342935da51c98cb3d3c8de507eb9d1d1f36ab5
a-kubotera <akira.kubotera+github@gmail.com>
2017-08-28 09:51:36 +0900

    動作テスト

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,14 +10,13 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-
+//= require rails-ujs
 //= require_tree .
 //= require_tree ./material-design
-//= require bootstrap-sprockets
-//= require bootstrap-material-design
-//= require rails-ujs
 //= require jquery2
 //= require jquery_ujs
+//= require bootstrap-material-design
+//= require bootstrap-sprockets
 //= require_self
 
 $(document).on('ready page:load', function() {
